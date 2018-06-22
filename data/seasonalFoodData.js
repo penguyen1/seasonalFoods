e1a5d93fe67f8168a82637b276b808f5cd7e299a
penguyen <peternguyen008@gmail.com>
2018-06-22 17:45:45 -0400

    update seasonal food data

--- data/seasonalFoodData.js
+++ data/seasonalFoodData.js
@@ -2,36 +2,61 @@ module.exports = [
   {
     season: "spring",
     foods: [
-      "blah blah blah Spring fact",
-      "one, two, three, spring.",
-      "ready, set, and... spring.",
+      "In the spring, Apricots should be soft to the touch and uniformly colored. Eat them as soon as possible, as they will not keep.",
+      "Between April and June, look for odorless Asparagus stalks with dry, tight tips. Avoid limp or wilted stalks.",
+      "Artichokes peak from March through May. Their leaves should be thick, tightly closed and have a vibrant deep green color.",
+      "Broccoli is great during the springtime. Look for odorless broccoli heads with compact clusters and bright bluish-green florets.",
+      "Fennels are delicious during the spring. Choose ones that have firm, white, unblemished fennel bulbs with bright green leaves.",
+      "Pineapples are best between March through July. It's ready if it smells like sweet pineapple juice when you sniff the butt of the pineapple.",
+      "From April through June, fresh and ripe rhubarbs should ideally have deep red flat stalks that are not curled or limp.",
+      "Watercress thrives throughout spring and summer. Look for perky dark-green leaves and avoid any that are yellowing, wilting, or slimy.",
+      "In Asia, Jackfruits ripen principally from March to June. Ripe ones will give off a sweet smell and give a little with gentle pressure.",
+      "Strawberries are at their sweetest between April and June. A ripe strawberry will be fragrant and red through and through.",
     ]
   },
   {
     season: "summer",
     foods: [
-      "Strawberries are at their sweetest between April through June. A ripe strawberry will be fragrant and red through and through.",
+      "Watermelon season runs throughout the summer. Tap the underbelly of the watermelon. A ripe one will have a deep hollow sound.",
       "Cucumbers are best between June and September. A ripe cucumber should be bright medium to dark green and firm.",
-      "Tomatoes ripen at different times based on its variety. Nonetheless, they should all be firm, but have a little bit of give when you squeeze it.",
-      "Bell pepper peak season runs from July through September. Their skins should be glossy, taut and unwrinkled, and their stems fresh and green.",
+      "Tomatoes ripen at different times based on its variety. Nonetheless, they should be firm, but have a bit of give when you squeeze it.",
+      "Bell peppers peak around July and September. Their skins should be glossy, taut and unwrinkled, and their stems fresh and green.",
       "Cherry season begins in early May and ends in late August. Mature cherries are firm and juicy, but tender enough to bite into easily.",
-      "Peaches are harvested from late June through August. A golden yellow color and medium-soft flesh when gently squeezed is a great indicator for ripeness.",
+      "Peaches are best between late June through August. A golden yellow color and medium-soft flesh when gently squeezed are great indicators for ripeness.",
+      "Throughout March to mid-August, ripe radishes are smooth, brightly colored and should have attached green and fresh looking tops.",
+      "In the summer, look for zucchini with a sightly prickly, but shinny skin. The skin should be firm and free of cuts and bruises.",
+      "Figs thrive during the summer season. They should be soft and yielding to the touch, but not mushy.",
+      "Shallots are best in the summer. They should be firm, heavy and have dry, papery skins. Avoid any signs of moisture or dark spots.",
     ]
   },
   {
     season: "fall",
     foods: [
-      "blah blah blah Fall fact",
-      "one, two, three, fall.",
-      "ready, set, and... fall.",
+      "Brussel sprouts are best during the fall season. Look for bright green heads that are firm and heavy with tightly packed leaves.",
+      "Autumn is the peak season for Cauliflower. Their heads should be compact, white, and firm and their leaves should be bright green.",
+      "Peak season for cranberries is October through December. The berry should be firm to the touch and have a dark red crimson color.",
+      "Fall is a special time for grapes. Ripe, healthy grapes should be firm and plump on all sides. Its stem should be green and flexible.",
+      "Kumquats are in season from November to March. Ripe kumquats range from bright orange to yellow-orange with firm and blemish-free skin.",
+      "Passion fruit season begins around the end of summer into fall. A ripe fruit will have dark purple shells that are dimpled and shriveled.",
+      "Persimmons are in season from October through February. Ripe persimmons should have smooth and blemish free skin with a bit of a give.",
+      "Pumpkin season occurs in the fall. Press your fingernail through the flesh. If it's not pierced easily and you have to work at it, it is ripe.",
+      "Turnips are best from October through March. The sweetest turnips are the size of golf balls with firm bodies, smooth skin and rich color.",
+      "Belgian endives are available from September to May. Look for the ones with crisp firmly packed heads with pale, yellow-green tips.",
     ]
   },
   {
     season: "winter",
     foods: [
-      "blah blah blah winter fact",
-      "one, two, three, winter.",
-      "ready, set, and... winter.",
+      "Winter Squashes are ready when their skin hardens. It should be non-glossy and rich in color without blemishes, cracks or soft spots.",
+      "Ginger is freshest during the winter. The skin should be shiny, thin and taut. You should be able to easily nick the skin with your fingernail.",
+      "Guavas are winter fruits that kicks off in November and lasts until April. It's in its prime when it has a yellowish-green color with a touch of pink.",
+      "Clementines are available from November until February. A ripe clementine should have glossy, soft skin with a uniform, bright-orange color all around.",
+      "Ruby red grapefruits run from November to May. Give the grapefruit a nice squeeze to see if it's firm. Firm all the way around is a good sign.",
+      "Kale thrives during the winter when temperatures drop. For tenderness and mild flavor, opt for smaller-leaved kale that are about the size of your hand.",
+      "Leeks are at their peak in January. In order to extract their full flavor, select smaller leeks that have as much white and light green as possible.",
+      "Pomegranate season typically runs from October through February. Look for a red to reddish-brown outer rind that is heavy for its size.",
+      "Sweet potatoes peak from late October through December. Small to medium sizes are sweet and creamy. The skin should be firm, smooth, and even-toned.",
+      "Tangerines are in season roughly from late October through January. Ripe tangerines will have an even, deep orange color and no soft or brown spots.",
     ]
   }
 ];
